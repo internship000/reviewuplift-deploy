e624f494abfb1a9bd201d69812d7ffd595a43f16
i-am-sam-c <samuelcb202004@gmail.com>
2025-06-11 09:33:05 +0530

    restart

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -21,23 +21,23 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyAfMVAj7G8gvM88_jdbMhvcEGxWuPBWk3Y",
-  authDomain: "reviewuplift-425c4.firebaseapp.com",
-  projectId: "reviewuplift-425c4",
-  storageBucket: "reviewuplift-425c4.firebasestorage.app",
-  messagingSenderId: "304773401426",
-  appId: "1:304773401426:web:7479742034cb725892088b",
-  measurementId: "G-B1FYXSQKC4"
+  // apiKey: "AIzaSyAfMVAj7G8gvM88_jdbMhvcEGxWuPBWk3Y",
+  // authDomain: "reviewuplift-425c4.firebaseapp.com",
+  // projectId: "reviewuplift-425c4",
+  // storageBucket: "reviewuplift-425c4.firebasestorage.app",
+  // messagingSenderId: "304773401426",
+  // appId: "1:304773401426:web:7479742034cb725892088b",
+  // measurementId: "G-B1FYXSQKC4"
  
 
   // akil
-  // apiKey: "AIzaSyCKQ8mKGOAZxUHyQjqu6V9vWVd6fPpxh1M",
-  // authDomain: "reviewuplift-testing.firebaseapp.com",
-  // projectId: "reviewuplift-testing",
-  // storageBucket: "reviewuplift-testing.firebasestorage.app",
-  // messagingSenderId: "256258929098",
-  // appId: "1:256258929098:web:f5f2b708d7b4bad6f79f33",
-  // measurementId: "G-NFQKY9YK7V"
+  apiKey: "AIzaSyCKQ8mKGOAZxUHyQjqu6V9vWVd6fPpxh1M",
+  authDomain: "reviewuplift-testing.firebaseapp.com",
+  projectId: "reviewuplift-testing",
+  storageBucket: "reviewuplift-testing.firebasestorage.app",
+  messagingSenderId: "256258929098",
+  appId: "1:256258929098:web:f5f2b708d7b4bad6f79f33",
+  measurementId: "G-NFQKY9YK7V"
 };
 
 const app = initializeApp(firebaseConfig);
